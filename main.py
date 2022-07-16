95def18dff194426e049e25c588fd776fabc023c
EduardoOsquel <eduardoosquel@hotmail.com>
2022-07-16 14:11:02 -0400

    cleanse code

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from ast import Lambda
-from fileinput import close
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtWidgets, uic
+from PyQt5.uic import loadUi
 import sys
-
-from youtube_dl import main
 
 class mainTunnel(QMainWindow):
     def __init__(self):
         super(mainTunnel, self).__init__()
-        uic.loadUi("gui/interface.ui", self)
+        loadUi("gui/interface.ui", self)
         
-        #app = QApplication(sys.argv)
-        
-        #window = uic.loadUi("gui/interface.ui")
-        #window.show()
-        
-        #window.actionExit.triggered.connect(self.actionExit)
         self.actionExit.triggered.connect(self.actionExit_Control)
-        #app.exec()         
         
     def actionExit_Control(self):
         sys.exit()
-        
-#mainTunnel()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
