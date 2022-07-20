2352bf774d5feb766c7d56ef1eae3ccf5caf8437
EduardoOsquel <eduardoosquel@hotmail.com>
2022-07-20 10:52:57 -0400

    read terminal

---
+++ readterminal.py
@@ -0,0 +1,16 @@
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+#command = 'ls -l'.split()
+#command = 'ls -l'.split()
+#for line in run_command(command):
+    #print(line)
+    
+#import subprocess
+output = subprocess.getoutput("ls -l")
+print(output)
